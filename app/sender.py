fdd7ee64a6e95d2d3ad07f9c30e4d0f8f8512eaf
Oleg-Pashchenko <pickpar@ya.ru>
2023-05-16 20:11:30 +0300

    d

--- app/sender.py
+++ app/sender.py
@@ -7,5 +7,5 @@ def get_answer(chat_history):
     url = 'https://api.suvvy.ai/api/v1/predict/chat/placeholder'
     response = requests.post(url,
                              data={
-                                 
+
                              })
