dc1cac729956eb9aa3a0b8df43445af5339b490c
Oleg-Pashchenko <pickpar@ya.ru>
2023-05-16 20:43:51 +0300

    d

--- app/sender.py
+++ app/sender.py
@@ -6,12 +6,19 @@ def get_answer(amo_messages):
     print(amo_messages[0], amo_messages[1])
     return 'success'
     for amo_message in amo_messages:
-        if amo_message:
+        if amo_message['author']['origin'] == 'amocrm':
             data.append({
                 'text': amo_message['text'],
                 'role': 'ai'
             })
-    return 'OK'
+        else:
+            data.append({
+                'text': amo_message['text'],
+                'role': 'human'
+            })
+    print(data)
     url = 'https://api.suvvy.ai/api/v1/predict/chat/placeholder'
-    response = requests.post(url,
-                             )
+    response = requests.post(url, data=data)
+    print('RESPONSE')
+    print(response.text)
+    return response.json()['prediction']
